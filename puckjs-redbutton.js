fcd338661bb772ed4bb473fca0b580e8928a33bd
Tobias Brunner <tobias.brunner@vshn.ch>
2024-06-02 21:09:36 +0200

    add espruino code for puckjs redbutton

---
+++ puckjs-redbutton.js
@@ -0,0 +1,23 @@
+var kb = require("ble_hid_keyboard");
+
+NRF.setAdvertising({}, {
+    name: 'RedButton',
+    interval: 2000
+});
+NRF.setServices(undefined, { hid: kb.report });
+
+NRF.nfcURL("https://vs.hn/boothraffle");
+
+function btnPressed() {
+    console.log("Button pressed!");
+    digitalWrite(LED1, 1);
+
+    setTimeout(function () {
+        digitalWrite(LED1, 0);
+    }, 500);
+
+    // Send the keyboard keys CTRL+ALT+G
+    kb.tap(kb.KEY.G, kb.MODIFY.CTRL | kb.MODIFY.ALT, function () { });
+}
+
+setWatch(btnPressed, BTN, { edge: "rising", repeat: true, debounce: 50 });
