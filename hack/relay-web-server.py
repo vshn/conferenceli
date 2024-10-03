9bf1d8677da9fb4858e575035a2419ffc03d867d
Tobias Brunner <tobias.brunner@vshn.ch>
2024-10-03 10:06:19 +0200

    relay server less amount of fog

--- hack/relay-web-server.py
+++ hack/relay-web-server.py
@@ -8,7 +8,8 @@ relay = OutputDevice(23, active_high=False)
 
 # Global variables to keep track of relay state and timer
 relay_active_time = 0
-max_relay_time = 20
+min_relay_time = 2
+max_relay_time = 10
 lock = threading.Lock()  # To manage concurrent access
 
 
@@ -46,7 +47,9 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Extend relay time
         with lock:
             if relay_active_time < max_relay_time:
-                additional_time = min(5, max_relay_time - relay_active_time)
+                additional_time = min(
+                    min_relay_time, max_relay_time - relay_active_time
+                )
                 relay_active_time += additional_time
                 print(
                     f"Relay time extended by {additional_time} seconds, total: {relay_active_time} seconds"
